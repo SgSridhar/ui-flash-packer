956d9312e8d5b173d3786ab68590b8311f3474de
neha <nehakaura243@gmail.com>
2017-05-06 18:02:15 +0530

    second screen

--- src/components/search-results.js
+++ src/components/search-results.js
@@ -2,7 +2,11 @@ import React from 'react'
 import R from 'ramda'
 
 import Header from './header'
-import flashpacker from '../assets/icons/flashpacker.png'
+import PopUpInfoWindowExample from './google-map'
+import yercaud from '../assets/icons/yercaud.png'
+import ooty from '../assets/icons/ooty.png'
+import kolli from '../assets/icons/kolli.png'
+import kodaikanal from '../assets/icons/kodaikanal.png'
 
 class SearchResults extends React.Component {
 	render() {
@@ -21,19 +25,85 @@ class SearchResults extends React.Component {
 						</div>
 						<div className="places-list">
 							<div className="place-card">
-								<div className="place-picture">Pic</div>
+								<div className="place-picture">
+									<img src={yercaud} width={200} height={200} alt="Yercaud" />
+								</div>
 								<div className="place-information">
 									<div className="name-rating">
 										<div className="place-name">Yercaud</div>
-										<div className="place-rating">3.5</div>
+										<div className="place-rating">
+											<span className="pt-icon-large pt-icon-star"></span>
+											<div>4.5</div>
+										</div>
 									</div>
-									<div className="distance">8.5 hours away</div>
-									<div className="tourist-influx">LOW</div>
+									<div className="distance">6.5 hours away</div>
+									<div className="tourist-influx">
+										<div className="label">Tourist Influx Level : </div>
+										<div>LOW</div>
+									</div>
+								</div>
+							</div>
+
+							<div className="place-card">
+								<div className="place-picture">
+									<img src={kolli} width={200} height={200} alt="Kolli" />
+								</div>
+								<div className="place-information">
+									<div className="name-rating">
+										<div className="place-name">Kolli Hills</div>
+										<div className="place-rating">
+											<span className="pt-icon-large pt-icon-star"></span>
+											<div>4.0</div>
+										</div>
+									</div>
+									<div className="distance">6.5 hours away</div>
+									<div className="tourist-influx">
+										<div className="label">Tourist Influx Level : </div>
+										<div>MEDIUM</div>
+									</div>
+								</div>
+							</div>
+							<div className="place-card">
+								<div className="place-picture">
+									<img src={ooty} width={200} height={200} alt="Ooty" />
+								</div>
+								<div className="place-information">
+									<div className="name-rating">
+										<div className="place-name">Ooty</div>
+										<div className="place-rating">
+											<span className="pt-icon-large pt-icon-star"></span>
+											<div>3.5</div>
+										</div>
+									</div>
+									<div className="distance">10 hours away</div>
+									<div className="tourist-influx">
+										<div className="label">Tourist Influx Level : </div>
+										<div>HIGH</div>
+									</div>
+								</div>
+							</div>
+							<div className="place-card">
+								<div className="place-picture">
+									<img src={kodaikanal} width={200} height={200} alt="kodaikanal" />
+								</div>
+								<div className="place-information">
+									<div className="name-rating">
+										<div className="place-name">Kodaikanal</div>
+										<div className="place-rating">
+											<span className="pt-icon-large pt-icon-star"></span>
+											<div>3.0</div>
+										</div>
+									</div>
+									<div className="distance">9.5 hours away</div>
+									<div className="tourist-influx">
+										<div className="label">Tourist Influx Level : </div>
+										<div>HIGH</div>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div className="map-view">mapview</div>
+					<div className="map-view"><PopUpInfoWindowExample/></div>
 				</div>
 			</div>
 		)
